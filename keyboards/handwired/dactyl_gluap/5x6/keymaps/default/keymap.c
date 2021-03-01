3c385794a9856646f1ec839cc1169a1b782f92ac
gluap <github@pgoergen.de>
2021-03-01 22:51:42 +0100

    Some more Optimization to make it easier to alt-tab. Also I really don't need the keypad.

--- keyboards/handwired/dactyl_gluap/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_gluap/5x6/keymaps/default/keymap.c
@@ -18,16 +18,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                          KC_Y  , KC_U  , KC_I  , KC_O  , KC_P   , DE_UDIA,
       DE_CIRC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                          KC_H  , KC_J  , KC_K  , KC_L  ,DE_ODIA , DE_ADIA,
       DE_LABK, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                          KC_N  , KC_M  ,DE_COMM,KC_DOT ,DE_MINS , DE_HASH,
-                      KC_LALT, KC_ESC,                                                          DE_PLUS, KC_RALT,
-                                        KC_BSPC,MO(_LOWER),                      KC_LGUI,  KC_ENTER,
-                                        KC_LSFT, MO(_RAISE),                        KC_RCTRL, KC_SPC
+                    MT(MOD_LALT,KC_DEL) , KC_ESC,                                                          DE_PLUS, KC_RALT,
+                                        MT(MOD_LALT,KC_DEL),MO(_RAISE),                      KC_LGUI,  MT(MOD_RALT,KC_ENTER),
+                                        MT(MOD_LSFT,KC_BSPC), MO(_LOWER),                        MT(MOD_LCTL,KC_TAB), KC_SPC
   ),      
   [_LOWER] = LAYOUT_5x6(
 
      KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, _______ ,_______ ,_______ ,_______,KC_PLUS,
+     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_HOME , KC_PGUP ,KC_PGDN ,KC_END,KC_PIPE,
+     _______,_______,_______,_______,_______,_______,                        _______, _______ ,_______ ,_______ ,KC_EQL ,KC_UNDS,
                                              _______,KC_PSCR,            _______, KC_P0,
 
                                              _______,_______,            _______,_______,

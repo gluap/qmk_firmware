8370340d9ff962869017cce908cfd5be812a3191
Your Name <you@example.com>
2021-06-29 22:42:01 +0200

    fix acute

--- keyboards/handwired/dactyl_gluap/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_gluap/5x6/keymaps/default/keymap.c
@@ -15,7 +15,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-      KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,  KC_6  ,                         KC_7  , KC_8  , KC_9  , KC_0  , DE_SS,   DE_GRV ,
+      KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,  KC_6  ,                         KC_7  , KC_8  , KC_9  , KC_0  , DE_SS,   DE_ACUT,
       KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                          KC_Y  , KC_U  , KC_I  , KC_O  , KC_P   , DE_UDIA,
       DE_CIRC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                          KC_H  , KC_J  , KC_K  , KC_L  ,DE_ODIA , DE_ADIA,
       DE_LABK, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                          KC_N  , KC_M  ,DE_COMM,KC_DOT ,DE_MINS , DE_HASH,

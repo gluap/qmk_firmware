98343ac7f8cb6d41e3bbd50ea77114e9fd2eaefa
gluap <github@pgoergen.de>
2024-04-01 19:27:37 +0200

    make caps lock and friends accessible

--- keyboards/handwired/dactyl_gluap/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_gluap/5x6/keymaps/default/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                          KC_Y  , KC_U  , KC_I  , KC_O  , KC_P   , DE_UDIA,
       DE_CIRC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                          KC_H  , KC_J  , KC_K  , KC_L  ,DE_ODIA , DE_ADIA,
       DE_LABK, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                          KC_N  , KC_M  ,DE_COMM,KC_DOT ,DE_MINS , DE_HASH,
-                    MT(MOD_LALT,KC_DEL) , MT(MOD_LCTL,KC_ESC),                                                          DE_PLUS, DE_BSLS,
+                    MT(MOD_LALT,KC_DEL) , MT(MOD_LCTL,KC_ESC),                                 DE_PLUS, DE_BSLS,
                                         MT(MOD_LALT,KC_DEL),MO(_RAISE),                      KC_LGUI,  MT(MOD_RALT,KC_ENTER),
                                         MT(MOD_LSFT,KC_BSPC), MO(_LOWER),                        MT(MOD_LCTL,KC_TAB), KC_SPC
   ),
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,_______,_______,_______,_______,DE_LBRC,                        DE_RBRC, _______ ,_______ ,_______ ,_______,KC_PLUS,
      _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,DE_LPRN,                        DE_RPRN, KC_HOME , KC_PGUP ,KC_PGDN ,KC_END,DE_PIPE,
      _______,_______,_______,_______,_______,DE_LCBR,                        DE_RCBR, _______ ,_______ ,_______ ,KC_EQL ,DE_UNDS,
-                                             _______,KC_PSCR,            _______, KC_P0,
+                    _______,KC_PSCR,                                                            _______, KC_P0,
 
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6(
        KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                        KC_F7  , KC_F8 , KC_F9 , KC_F10 ,KC_F11 ,KC_F12,
-       _______,_______,_______,_______,_______,DE_LBRC,                        DE_RBRC,_______,_______,KC_INS ,_______,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,DE_LPRN,                        DE_RPRN,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_VOLU,
-       _______,_______,_______,_______,_______,DE_LCBR,                        DE_RCBR,_______,_______,_______,_______,KC_VOLD,
-                                               _______,_______,            KC_EQL ,_______,
+       KC_CAPS,_______,_______,_______,_______,DE_LBRC,                       DE_RBRC,_______,_______,KC_INS ,_______,KC_MUTE,
+       KC_NUM ,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,DE_LPRN,                       DE_RPRN,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_VOLU,
+       KC_SCRL,_______,_______,_______,_______,DE_LCBR,                       DE_RCBR,_______,_______,_______,_______,KC_VOLD,
+                       _______,_______,                                                        KC_EQL ,_______,
 
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______
@@ -120,10 +120,10 @@ oled_set_cursor(0, 0);
             oled_write_P(PSTR("        "), false);
             break;
         case _RAISE:
-            oled_write_P(PSTR("PgupDown"), false);
+            oled_write_P(PSTR("Arrows"), false);
             break;
         case _LOWER:
-            oled_write_P(PSTR("Arrows"), false);
+            oled_write_P(PSTR("PGUPDOWN"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
